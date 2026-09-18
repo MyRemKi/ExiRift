da28ad83886cae355b5dabc15ac53594d8a319ce
MyRemKi <remi.kalkan01@hotmail.com>
2026-09-18 21:33:11 +0200

    commit change

---
+++ .SFML_Part/Process/ProcessEvent.h
@@ -0,0 +1,17 @@
+#ifndef PROCESSEVENT_H
+#define PROCESSEVENT_H
+#include <SFML/Graphics.hpp>
+
+#pragma once
+
+class ProcessEvent
+{
+public:
+    ProcessEvent();
+    ~ProcessEvent();
+
+private:
+
+};
+
+#endif
